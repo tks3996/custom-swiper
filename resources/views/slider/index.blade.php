63c98a303d8b68d896a89597e185636bf4a993bd
Tushar <tusharakanta.s@theswipewire.in>
2025-02-15 12:17:36 +0530

    url added

--- resources/views/slider/index.blade.php
+++ resources/views/slider/index.blade.php
@@ -5,14 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Customizable Image Slider - Fast & Responsive</title>
     <meta name="description" content="A highly customizable, lightweight, and performance-optimized image slider built with Vanilla JavaScript.">
-
-    <link rel="stylesheet" href="{{asset('slider/css/styles.css')}}">
+    
+    {{-- <link rel="stylesheet" href="{{asset('slider/css/styles.css')}}"> --}}
+    <link rel="stylesheet" href="{{ url('/slider/css/styles.css') }}">
 
     <link rel="preload" as="image" href="/slider/images/1.webp" type="image/webp">
 </head>
 <body>
     <div id="custom-carousel"></div>
 
-    <script type="module" src="{{asset('slider/js/main.js')}}"></script>
+    {{-- <script type="module" src="{{asset('slider/js/main.js')}}"></script> --}}
+
+<script type="module" src="{{ url('/slider/js/main.js') }}"></script>
+
 </body>
 </html>
