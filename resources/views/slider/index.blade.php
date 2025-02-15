27ce6ebdfb22846bb820ffe948b29063d105bcac
Tushar <tusharakanta.s@theswipewire.in>
2025-02-15 12:35:16 +0530

    fix env

--- resources/views/slider/index.blade.php
+++ resources/views/slider/index.blade.php
@@ -6,14 +6,14 @@
     <title>Customizable Image Slider - Fast & Responsive</title>
     <meta name="description" content="A highly customizable, lightweight, and performance-optimized image slider built with Vanilla JavaScript.">
 
-    <link rel="stylesheet" href="./slider/css/styles.css">
+    <link rel="stylesheet" href="{{asset('slider/css/styles.css')}}">
 
     <link rel="preload" as="image" href="/slider/images/1.webp" type="image/webp">
 </head>
 <body>
     <div id="custom-carousel"></div>
 
-    <script type="module" src="./slider/js/main.js"></script>
+    <script type="module" src="{{asset('slider/js/main.js')}}"></script>
 
     
 </body>
